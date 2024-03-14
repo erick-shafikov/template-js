5aec959a2cc0b47debca88f6146c906062eebc99
erick-shafikov <100657557+erick-shafikov@users.noreply.github.com>
2024-03-14 16:40:53 +0300

    ini commit

---
+++ _nodejs.js
@@ -0,0 +1,16 @@
+const readline = require('readline');
+
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+
+let input = [];
+let res;
+
+rl.on('line', (inputItem) => {});
+
+rl.on('close', () => {
+  console.log(res);
+  process.exit(0);
+});
